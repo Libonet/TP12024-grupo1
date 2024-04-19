991f040294c9a7ccbb69a97cb6fe5dc7dad50edb
Santiago Libonati <santiago.libonati@gmail.com>
2024-04-19 14:05:33 -0300

    Cambiados los datos que se importan

--- importarDatos.R
+++ importarDatos.R
@@ -3,7 +3,7 @@ library(readxl)
 
 datosLP <- read_xlsx("Datos_LP.xlsx", col_names = FALSE)
 
-datosLP <- datosLP[ -c(4:23, 61:118) ]
+datosLP <- datosLP[ -c(5:23, 61:118) ]
 
 datosLP[3, 2] = "Provincia"
 datosLP[3, 3] = "Barrio"
@@ -12,9 +12,9 @@ tabla <- datosLP[ -c(1, 2), ]
 
 write.csv(tabla, file="tabla_datos.csv")
 
-prueba <- read.csv("tabla_datos.csv", skip = 1, stringsAsFactors = TRUE)
-prueba <- prueba [ ,-c(1,2) ]
+tabla <- read.csv("tabla_datos.csv", skip = 1, stringsAsFactors = TRUE)
+tabla <- tabla [ ,-c(1,2) ]
 
-write.csv(prueba, "datosLP_limpios.csv")
+write.csv(tabla, "datosLP_limpios.csv")
 file.remove("tabla_datos.csv")
 
